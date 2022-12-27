fa05cb4279ac60fa740c26612f8c7a4ce9b42cb8
diken.dev <diken.dev@gmail.com>
2022-12-27 01:05:52 -0300

    added users to database using java JDBC

--- User-Management/src/management/Main.java
+++ User-Management/src/management/Main.java
@@ -2,21 +2,42 @@ package management;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
 	public static void main(String[] args) {
 		String jdbcUrl= "jdbc:mysql://localhost:3306/sampledb";
-		String username = "root";
-		String password = "pass";
+		String dbUsername = "root";
+		String dbPassword = "pass";
+		
+		String username = "Tuntun";
+		String password = "0707";
+		String fullname = "Cristina Freitas de Souza";
+		String email = "Cris@cris.com";
 		
 		try {
-			Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-			if (connection != null) {
-				System.out.println("connected to database!!");
-				connection.close();
+			Connection connection = DriverManager.getConnection(jdbcUrl, dbUsername, dbPassword);
+		
+			String sql = "INSERT INTO users (username, password, fullname, email)" 
+			+ "VALUES(?,?,?,?)";
+			
+			
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1,username);
+			statement.setString(2, password);
+			statement.setString(3, fullname);
+			statement.setString(4, email);
+				
+			int rows = statement.executeUpdate();
+			
+			if(rows > 0) {
+				System.out.println("added row successfully");
 			}
+			
+			connection.close();
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
